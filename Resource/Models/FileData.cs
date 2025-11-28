90e3b956e111b17ce9f12eefd19ca7c1ef811a5c
Ava <66749335+Ava-UwU@users.noreply.github.com>
2025-11-28 14:24:07 +0330

    added file data struct

---
+++ Resource/Models/FileData.cs
@@ -0,0 +1,14 @@
+﻿namespace HexWorldUtils.Resource.Models
+{
+    public class FileData<T>
+    {
+        public readonly T File;
+        public readonly string FileName;
+
+        public FileData(T file, string fileName)
+        {
+            File = file;
+            FileName = fileName;
+        }
+    }
+}
