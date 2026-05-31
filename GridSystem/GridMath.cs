f5f9fae94c772d66011c86a94e522363c6693e85
Ava <66749335+Ava-UwU@users.noreply.github.com>
2026-05-31 11:22:45 +0330

    added grid point rotation

--- GridSystem/GridMath.cs
+++ GridSystem/GridMath.cs
@@ -18,7 +18,7 @@ namespace HexWorldUtils.GridSystem
             var offset = worldPosition - originPosition;
             return new int2((int)math.floor(offset.x / cellSizeX), (int)math.floor(offset.z / cellSizeY));
         }
-        
+
         [MethodImpl(MethodImplOptions.AggressiveInlining)]
         public static float3 GridToWorld2D(float3 origin, float2 cellSize, int x, int y)
         {
@@ -33,6 +33,58 @@ namespace HexWorldUtils.GridSystem
             var worldX = origin.x + (x * cellSize.x);
             var worldZ = origin.z + (y * cellSize.y);
             return new float3(worldX, origin.y, worldZ);
+        }
+
+        [MethodImpl(MethodImplOptions.AggressiveInlining)]
+        public static int2 RotateRight(int2 p, int rotationStep)
+        {
+            const int cx = 500;
+            const int cy = 500;
+            int k = (-rotationStep) % 6;
+            if (k < 0)
+                k += 6;
+
+            int dx = p.x - cx;
+            int dy = p.y - cy;
+
+            float cos, sin;
+            switch (k)
+            {
+                case 0:
+                    cos = 1.0;
+                    sin = 0.0;
+                    break;
+                case 1:
+                    cos = 0.5;
+                    sin = Math.Sqrt(3) / 2f;
+                    break;
+                case 2:
+                    cos = -0.5;
+                    sin = Math.Sqrt(3) / 2f;
+                    break;
+                case 3:
+                    cos = -1.0;
+                    sin = 0.0;
+                    break;
+                case 4:
+                    cos = -0.5;
+                    sin = -Math.Sqrt(3) / 2f;
+                    break;
+                case 5:
+                    cos = 0.5;
+                    sin = -Math.Sqrt(3) / 2f;
+                    break;
+                default:
+                    throw new InvalidOperationException();
+            }
+
+            float rx = dx * cos - dy * sin;
+            float ry = dx * sin + dy * cos;
+
+            int x2 = (int)Math.Round(rx) + cx;
+            int y2 = (int)Math.Round(ry) + cy;
+
+            return new int2(x2, y2);
         }
     }
 }
